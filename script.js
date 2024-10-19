d11c7e723af6a736fc0145266c35a2448dff232f
PlatypusBuddyMan <87823152+PlatypusBuddyMan@users.noreply.github.com>
2024-10-19 05:09:58 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('toggle-button').addEventListener('click', function() {
+    const stylesheet = document.getElementById('theme-stylesheet');
+    if (stylesheet.getAttribute('href') === 'style1.css') {
+        stylesheet.setAttribute('href', 'style2.css');
+    } else {
+        stylesheet.setAttribute('href', 'style1.css');
+    }
+});
