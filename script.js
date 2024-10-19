d62ddfd4047018051d7cd08cc78b25cade3b2f66
PlatypusBuddyMan <87823152+PlatypusBuddyMan@users.noreply.github.com>
2024-10-19 05:18:28 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    const stylesheet = document.getElementById('theme-stylesheet');
-    if (stylesheet.getAttribute('href') === './style1.css') {
-        stylesheet.setAttribute('href', './style2.css');
-    } else {
-        stylesheet.setAttribute('href', './style1.css');
-    }
-});
